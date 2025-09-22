ce1b467c558b00e40f23b1b28a3de0b81b60e2db
yousef <yousef.alogiely@gmail.com>
2025-09-22 13:00:08 +0300

    lap 4

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
-urlpatterns = [ 
-            path('', views.index), 
-            path('index2/<int:val1>/', views.index2),
-            path('<int:bookId>', views.viewbook)
+urlpatterns = [
+            # ---------------- LAB 3 ------------------- 
+            # path('', views.index), 
+            # path('index2/<int:val1>/', views.index2),
+            # path('<int:bookId>', views.viewbook)
+            
+            
+            # ---------------- LAB 4 -------------------
+            path('', views.index, name='books.index'),
+            path('list_books/', views.list_books, name='books.list_books'),
+            path('<int:bookId>/', views.viewbook, name='books.view_one_book'),
+            path('aboutus/',views.aboutus,name='books.aboutus')
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -2,22 +2,42 @@ from django.shortcuts import render,redirect
 
 from django.http import HttpResponse
 
-def index(request): 
-    name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html", {"name":name})
+
+## ------------------------ LAB 3 ----------------------------
+# def index(request): 
+#     name = request.GET.get("name") or "world!"
+#     return render(request, "bookmodule/index.html", {"name":name})
 
 
-def index2(request, val1 = 0): #add the view function (index2) 
-    return HttpResponse("value1 = "+str(val1))
+# def index2(request, val1 = 0): #add the view function (index2) 
+#     return HttpResponse("value1 = "+str(val1))
 
 
-def viewbook(request, bookId):
-    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
-    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'} 
-    targetBook = None 
-    if book1['id'] == bookId: targetBook = book1
-    elif book2['id'] == bookId: targetBook = book2
-    else:
-        return redirect('https://github.com/Yousef10p')
-    context = {'book':targetBook} 
-    return render(request, 'bookmodule/show.html', context)
+# def viewbook(request, bookId):
+#     book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+#     book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'} 
+#     targetBook = None 
+#     if book1['id'] == bookId: targetBook = book1
+#     elif book2['id'] == bookId: targetBook = book2
+#     else:
+#         return redirect('https://github.com/Yousef10p')
+#     context = {'book':targetBook} 
+#     return render(request, 'bookmodule/show.html', context)
+
+## ------------------------ LAB 4 ----------------------------
+def index(request):
+    return render(request,'bookmodule/index.html')
+
+def list_books(request):
+    return render(request,'bookmodule/list_books.html')
+
+
+def viewbook(request,bookId):
+    return render(request,'bookmodule/one_book.html')
+
+
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+
+
+
